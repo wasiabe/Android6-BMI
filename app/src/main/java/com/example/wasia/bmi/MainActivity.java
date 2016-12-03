8d4a8a6349fe74226b7692f04c1d540bd8ec0c0e
wasia <wasiabe@gmail.com>
2016-12-03 20:41:06 +0800

    Multiple language & event listener

--- app/src/main/java/com/example/wasia/bmi/MainActivity.java
+++ app/src/main/java/com/example/wasia/bmi/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.wasia.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViews();
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(R.string.calculate_bmi)
+                        .setMessage(R.string.bmi_definition)
+                        .setPositiveButton(R.string.ok, null)
+                        .show();
+            }
+        });
+
     }
 
     private void findViews() {
@@ -27,12 +40,26 @@ public class MainActivity extends AppCompatActivity {
         btnHelp = (Button) findViewById(R.id.btnHelp);
     }
 
-    public void calBmi (View v) {
+    public void calBmi (View v) throws Exception{
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
-        double weight = Double.parseDouble(w);
-        double height = Double.parseDouble(h);
-        double bmi = weight/Math.pow(height,(double)2);
-        Log.d("BMI",String.valueOf(bmi));
+        try {
+            double weight = Double.parseDouble(w);
+            double height = Double.parseDouble(h);
+            double bmi = weight / Math.pow(height, (double) 2);
+            Log.d("BMI", String.valueOf(bmi));
+            String msgBmi = "Your BMI is " + String.valueOf((bmi));
+            Toast.makeText(this, msgBmi, Toast.LENGTH_LONG).show();
+            AlertDialog builder = new AlertDialog.Builder(this)
+                    .setMessage("Your BMI is " + String.valueOf(bmi))
+                    .setPositiveButton("OK", null)
+                    .setTitle("BMI")
+                    .show();
+
+        } catch (Exception e) {
+            String msgErr = e.getMessage();
+            Toast.makeText(this, msgErr, Toast.LENGTH_LONG).show();
+        }
+
     }
 }
